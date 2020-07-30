230d8b6003e9714c1e5c0a5878bf2a9a19d7114c
shu-wan9 <18762750392@163.com>
2020-07-30 22:49:19 +0800

    use Context

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,22 @@
-import Koa from 'koa'
+import Koa, { Context } from 'koa'
 
 const app = new Koa()
 
-app.use(async (ctx, next) => {
+app.use(async (ctx: Context, next) => {
   // await next()
-  ctx.body = 'hi~'
+  // ctx.body = 'hi~'
+
+  ctx.body = {
+    author: 'shuwan9'
+  }
+
+  ctx.set({
+    'Content-Type': 'text/plain'
+  })
 })
-app.use(async (ctx) => {
-  ctx.res.setHeader('X-Author-Name', 'shuwan9')
+app.use(async (ctx: Context) => {
+  ctx.set('x-author-name', 'shuwan9')
+  ctx.set({ 'x-author-age': '18', 'x-anthor-sex': 'male' })
 })
 
 app.use(async (ctx) => {})
